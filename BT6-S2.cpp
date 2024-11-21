5934422aa8b7de3170ca7bf1b340bcc1313a2e84
letrungchien <letrungchien195@gmail.com>
2024-11-21 10:13:46 +0700

    Update BT6-S2.cpp

--- BT6-S2.cpp
+++ BT6-S2.cpp
@@ -2,14 +2,14 @@
 
 int main() {
     const float PI = 3.14; 
-    int banKinh = 5;       
-    printf("Gia tri ban kinh hinh tron = %d \n", banKinh);
+    int ban_Kinh = 6;       
+    printf("Gia tri ban kinh hinh tron = %d \n", ban_Kinh);
 
-    float chuVi = 2 * PI * banKinh;
-    printf("Chu vi hinh tron = %.2f \n", chuVi);
+    float chu_Vi = 2 * PI * ban_Kinh;
+    printf("Chu vi hinh tron = %.2f \n", chu_Vi);
 
-    float dienTich = PI * banKinh * banKinh;
-    printf("Dien tich hinh tron = %.2f \n", dienTich);
+    float dien_Tich = PI * ban_Kinh * ban_Kinh;
+    printf("Dien tich hinh tron = %.2f \n", dien_Tich);
 
     return 0;
 }
